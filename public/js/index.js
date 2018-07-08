bf582ce2f321f372a8f5755f96d6bdaaeee2dbfb
sebastiankull <s.kull@iftra.de>
2018-07-08 22:18:36 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        'from': 'Sebastian',
-        'message': 'Hey. Whats up?'
-    });
 });
 
 socket.on('disconnect', function () {
